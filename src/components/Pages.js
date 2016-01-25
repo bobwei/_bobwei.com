e5163ba1977d0d2b62884dd25dc9bf8d153e5451
bobwei <bob61016@gmail.com>
2016-01-25 22:25:14 +0800

    add settour app

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classnames from 'classnames';
 
 import { nl2br } from '../utils';
 
@@ -10,11 +11,11 @@ export default class Pages extends React.Component {
         {this.props.pages.map((page, i) => {
           return (
             <div className="container-fluid" key={i}>
-              <div className="row">
-                <div className="col-md-5 page center" style={page.coverImageStyles}>
+              <div className={classnames('row', { 'rtl': page.order === 'rtl' })}>
+                <div className="col-md-5 page center" style={page.subpage0Styles}>
                   <img className="cover-image" src={page.coverImage} />
                 </div>
-                <div className="col-md-7 page center color-background" style={page.contentStyles}>
+                <div className="col-md-7 page center color-background" style={page.subpage1Styles}>
                   <div className="content">
                     <h1>
                       {nl2br(page.contentTitle)}
@@ -40,10 +41,10 @@ Pages.defaultProps = {
         return require('../images/mosorder.jpg');
       }
     })(),
-    coverImageStyles: {
+    subpage0Styles: {
       backgroundColor: 'white'
     },
-    contentStyles: {
+    subpage1Styles: {
       backgroundColor: '#CC3000'
     },
     contentTitle: 'MOS Order\n摩斯漢堡行動點餐 App',
@@ -51,6 +52,25 @@ Pages.defaultProps = {
       每天超過 5000 筆行動訂單，
       使用者評價高於 4 顆星，
       優質服務從不間斷，美味幸福在眼前。
+    `
+  }, {
+    order: 'rtl',
+    coverImage: (() => {
+      if (process.env.BROWSER) {
+        return require('../images/settour.jpg');
+      }
+    })(),
+    subpage0Styles: {
+      backgroundColor: 'white'
+    },
+    subpage1Styles: {
+      backgroundColor: '#30BCFC'
+    },
+    contentTitle: '旅遊+\n東南旅遊 App',
+    description: `全方位旅遊服務整合，
+    即時查詢、即時商品訂購，
+    使用者評價高於 4 顆星，
+    不論您在哪裡，輕鬆訂購好 Fun 心！
     `
   }]
 };
