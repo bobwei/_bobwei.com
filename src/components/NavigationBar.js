eed6d6512cbb11a20deb75c9d00a29a2f68e6d64
bobwei <bob61016@gmail.com>
2016-01-25 15:34:03 +0800

    add NavigationBar and CoverPhoto

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import classNames from 'classnames';
+import { Link } from 'react-router';
+
+
+class NavigationBar extends React.Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-transparent navbar-fixed-top">
+        <div className="container">
+          <div className="navbar-header">
+            <Link to="/" className="navbar-brand">
+              Bob Wei ( 韋伯俞 )
+            </Link>
+          </div>
+          <div className="collapse navbar-collapse">
+            <ul className="nav navbar-nav right">
+              <li className={classNames({'active': this.context.router.isActive({pathname: '/products'})})}>
+                <Link to="/products">
+                  Products
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+NavigationBar.defaultProps = {
+};
+
+NavigationBar.contextTypes = {
+  router: React.PropTypes.object.isRequired
+};
+
+export default NavigationBar;
--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import NavigationBar from '../components/NavigationBar';
+
 
 class MainPage extends React.Component {
   render() {
     return (
       <div>
+        <NavigationBar />
+        <div className="cover-photo">
+          <h1>
+            Build Something Users Love !
+          </h1>
+        </div>
       </div>
     );
   }
