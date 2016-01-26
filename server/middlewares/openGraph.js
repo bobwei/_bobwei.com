4dd840471bf7ed1b7f206b9e00ef66770e523f19
bobwei <bob61016@gmail.com>
2016-01-26 10:48:51 +0800

    bug fixed

--- server/middlewares/openGraph.js
+++ server/middlewares/openGraph.js
@@ -6,7 +6,7 @@ export default function() {
     let baseUrl = req.protocol + '://' + req.get('host');
     req.openGraph = {
       type: 'website',
-      title: 'Bob Wie ( 韋伯俞 )',
+      title: 'Bob Wei ( 韋伯俞 )',
       description: 'Linkwish 共同創辦人、創業家與程式設計師，致力於打造令人驚艷的產品。',
       image: `${baseUrl}/assets/cover-photo.jpg`
     };
