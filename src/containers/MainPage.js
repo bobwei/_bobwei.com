d8e4e2a3a67dd4d8e119bf36676f88b81125e3a3
bobwei <bob61016@gmail.com>
2016-01-27 13:59:38 +0800

    add products-link

--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import jQuery from 'jquery';
 
 import NavigationBar from '../components/NavigationBar';
 import Pages from '../components/Pages';
 
+if (process.env.BROWSER) {
+  require('../styles/MainPage.scss');
+}
 
 class MainPage extends React.Component {
+
+  onProductsLinkClick() {
+    let coverPhotoHeight = jQuery('.cover-photo').height();
+    jQuery('html, body').animate({
+      scrollTop: coverPhotoHeight - 100
+    }, 450);
+  }
 
   render() {
     let coverPhoto;
@@ -17,12 +28,21 @@ class MainPage extends React.Component {
         <NavigationBar />
         <div
           className="cover-photo center"
-          style={{ backgroundImage: `url(${coverPhoto})` }}
+          style={{
+            backgroundImage: `url(${coverPhoto})`,
+            position: 'relative'
+          }}
         >
           <div className="gradient-content">
             <h1>
               Build Something Users Love !
             </h1>
+          </div>
+          <div className="products-link" onClick={this.onProductsLinkClick.bind(this)}>
+            <h2>
+              案例作品
+            </h2>
+            <i className="fa fa-chevron-down" />
           </div>
           <div className="gradient" />
         </div>
